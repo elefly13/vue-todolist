6de9d8cd812f3efbb29d81c04cf9548531f8b1f8
Elefly13 <elymara13@gmail.com>
2021-09-09 15:57:34 +0200

    struttura pagina html

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 Vue.config.devtools = true;
+
+const app = new Vue(
+    {
+        el: '#root',
+        data: {
+            toDos: [
+                "fare i compiti",
+                "fare la spesa",
+                "fare il letto",
+            ]
+        }
+    }
+)
