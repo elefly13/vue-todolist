497fb2363b301fcc52f9bf1eb0343c909ed8d968
Elefly13 <elymara13@gmail.com>
2021-09-09 22:29:46 +0200

    Terza logica + semiBonus

--- js/main.js
+++ js/main.js
@@ -6,22 +6,40 @@ const app = new Vue(
         data: {
             newToDo: "",
             toDos: [
-                "Fare i compiti",
-                "Fare la spesa",
-                "Fare il letto",
+                // "Fare i compiti",
+                // "Fare la spesa",
+                // "Fare il letto",
+                {
+                    titolo: "fare i compiti",
+                    fatto: false,
+                    colorClass: ".red"
+                },
+                {
+                    titolo: "fare la spesa",
+                    fatto: false,
+                    colorClass: ".red"
+                },
+                {
+                    titolo: "fare il bucato",
+                    fatto: false,
+                    colorClass: ".red"
+                }
             ]
         },
         methods: {
             addToDo() {  
                 if(this.newToDo != "") {
-                    this.toDos.push(this.newToDo);
+                    // this.toDos.push(this.newToDo); //Array
+                    this.toDos.push({titolo: this.newToDo}); //Oggetti
                     this.newToDo = "";
                 }
-                
             },
             removeToDo(index) {
                 this.toDos.splice(index,1);
-            }
+            },
+            changeColor() {
+                (this.toDos.fatto == false) ? this.toDos.colorClass = ".green" : this.toDos.colorClass = ".red";
+            },
         }
     }
 )
